83bb5c340e67cf8434af56125aefa357c4b20cb0
hiroto <yossy.goodcolor@gmail.com>
2018-03-07 08:18:13 +0000

    have修正

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -33,6 +33,10 @@ class OwnershipsController < ApplicationController
       flash[:success] = '商品の Want を解除しました。'
     end
 
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
+    end
     redirect_back(fallback_location: root_path)
   end
 end
